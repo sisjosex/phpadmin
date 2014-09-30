b72652cbc829350d53229a7b40a8db6f8ba4b5b9
sisjosex <sisjosex@gmail.com>
2014-09-30 10:11:30 -0400

    documentation

--- README.md
+++ README.md
@@ -2,3 +2,27 @@ phpadmin
 ========
 
 Nice and easy admin for generate backend and implement frontend side also.
+
+Requirements
+--------------
+
+- Rewrite module enabled, for example on ubuntu, apache2:
+
+`$sudo a2enmod rewrite`
+`$sudo service apache2 restart`
+
+Installation
+--------------
+
+- Copy files and folders inside some directory, for example "project1".
+- Edit `config/constants.php` #8
+
+`define('BASE_URL', $protocol . ':' . '//' . $_SERVER['SERVER_NAME'] . $port . '/project1/');`
+
+- Edit Database config `config/database.php`
+
+- Load in browser depending your environment maybe: `http://localhost/phpadmin`, `http://localhost/admin`
+- Admin Credentials
+
+`admin@gmail.com`
+`admin`
--- config/constants.php
+++ config/constants.php
@@ -5,13 +5,13 @@ $port = $_SERVER['SERVER_PORT'];
 
 $port = ($port != '80') ? ':' . $port : '';
 
-define('BASE_URL', $protocol . ':' . '//' . $_SERVER['SERVER_NAME'] . $port . '/test/framework2/');
+define('BASE_URL', $protocol . ':' . '//' . $_SERVER['SERVER_NAME'] . $port . '/test/phpadmin/');
 $constants['BASE_URL'] = BASE_URL;
 
 /* Default controllers by folder name*/
 $default_controllers['admin'] = 'login';
 
-define('FACEBOOK_URL','https://www.facebook.com/PremierFitnessClub');
+define('FACEBOOK_URL','https://www.facebook.com/');
 
 /* Default controller name for default site (frontend) */
 define('DEFAULT_CONTROLLER', 'home');
@@ -23,17 +23,17 @@ define('DEFAULT_FUNCTION', 'index');
 define('AUTH_COOKIE_EXPIRE', 3600*60*24);
 
 /* Cookie name for store session id */
-define('AUTH_COOKIE_NAME', '_framework2');
+define('AUTH_COOKIE_NAME', '_phpadmin');
 
 /* Database profix for all tables */
 define('DB_PREFIX', '');
 
 
 /* Email from header Title */
-define('MAIL_FROM_TITLE', 'Premier Group');
+define('MAIL_FROM_TITLE', 'Email From Title');
 
 /* Email from address to show */
-define('MAIL_FROM_ADDRESS', 'info@premier.com.bo');
+define('MAIL_FROM_ADDRESS', 'info@domain.com');
 
 /* Admin email for send copy emails */
-define('MAIL_ADMIN_ADDRESS', 'sisjosex@gmail.com');
+define('MAIL_ADMIN_ADDRESS', 'admin@domain.com');
